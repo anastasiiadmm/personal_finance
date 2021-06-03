069ee6bf5b5131f7af22ab9cbf9aede808d043f3
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2021-06-03 18:24:58 +0600

    #11 создана сущность Transaction

---
+++ api/models/Transaction.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+  const Transaction = sequelize.define('Transaction', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    accountOut: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Account',
+        key: 'id'
+      }
+    },
+    accountIn: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Account',
+        key: 'id'
+      }
+    },
+    sumOut: {
+      type: DataTypes.INTEGER,
+    },
+    sumIn: {
+      type: DataTypes.INTEGER,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    description: DataTypes.STRING,
+    cashierCheck: DataTypes.STRING,
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+    tableName:'transaction'
+  });
+
+  return Transaction;
+};
