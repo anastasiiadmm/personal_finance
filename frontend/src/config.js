f26d8595d56c8fcfc4ac4dd774da98e02b86b32d
anastasiiapanama <kilma.1334@gmail.com>
2021-07-03 16:49:56 +0600

    #40 - изменила настройку конфигурации подключения к порту во frondend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,10 @@
-export const apiURL = 'http://localhost:8000';
+const env = process.env.REACT_APP_ENV;
+
+let domain = 'http://localhost:8010';
+
+if (env === 'test') {
+    domain = 'http://localhost:8010';
+}
+
+export const apiURL = domain;
 export const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
