560b53aefdff280943a690f82989a4ab667b7abc
anastasiiapanama <kilma.1334@gmail.com>
2021-07-11 15:50:10 +0600

    #40 - Generated steps.js

--- tests/step_definitions/steps.js
+++ tests/step_definitions/steps.js
@@ -1,48 +1,27 @@
 const { I } = inject();
 
 Given('я нахожусь на странице регистрации', () => {
-  I.amOnPage('/signup');
-});
-
-Given('я нахожусь на странице логина', () => {
-  I.amOnPage('/login');
+    I.amOnPage('/signup');
 });
 
 When('я ввожу в поля текст:', table => {
-  for (const id in table.rows) {
-    if (id < 1) {
-      continue;
+    for (const id in table.rows) {
+        if (id < 1) {
+            continue;
+        }
+
+        const cells = table.rows[id].cells;
+        const field = cells[0].value;
+        const value = cells[1].value;
+
+        I.fillField(field, value);
     }
-
-    const cells = table.rows[id].cells;
-    const field = cells[0].value;
-    const value = cells[1].value;
-
-    I.fillField(field, value);
-  }
 });
 
 When('нажимаю на кнопку Sign up', () => {
-  I.click(`//button[contains(@id,"signup-button")]`);
-});
-
-When('нажимаю на кнопку Sign in', () => {
-  I.click(`//button[contains(@id,"signin-button")]`);
+    I.click(`//button[contains(@id,"signup-button")]`);
 });
 
 Then('вижу {string}', str => {
-  I.see(str);
+    I.see(str);
 });
-
-Then('попадаю на страницу списка всех групп', () => {
-  I.amOnPage('/groups');
-});
-
-Then('я нажимаю на кнопку Add new group', () => {
-  I.click(`//button//*[contains(text(),"Add new group")]/..`);
-});
-
-When('нажимаю на кнопку Create', () => {
-  I.click(`//button//*[contains(text(),"Create")]/..`);
-});
-
