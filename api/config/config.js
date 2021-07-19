080eaeda7b2c754aa85f6972b010e53b59608104
anastasiiapanama <kilma.1334@gmail.com>
2021-07-19 16:01:08 +0600

    #45 - updated dependencies

--- api/config/config.js
+++ api/config/config.js
@@ -44,7 +44,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: databaseUrl,
     host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectOptions: {
